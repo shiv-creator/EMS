57d7387a0a07d7ca66292c82007536685211a782
Ankit1712 <64196775+Ankit1712@users.noreply.github.com>
2020-05-14 04:36:30 +0530

    Update ProjectTest.py

--- pythonTest/ProjectTest.py
+++ pythonTest/ProjectTest.py
@@ -8,7 +8,7 @@ location = "Noida"
 driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver',options=options)
 #driver = webdriver.Firefox(firefox_options=options);
 driver.get("http://192.168.0.5:8080/EmployeeManagementSystem/project.jsp")
-assert "Employee Management System" in driver.title
+assert "Registration" in driver.title
 elem = driver.find_element_by_name("name")
 elem.send_keys(name)
 elem = driver.find_element_by_name("location")
